804ccd85e291945e879f2853e8bad576582ae57c
nsdavian <nsdavenorth@gmail.com>
2023-12-14 13:10:16 +0100

    v1.2

--- src/components/CreateNews.jsx
+++ src/components/CreateNews.jsx
@@ -1,7 +1,48 @@
 import { Link } from 'react-router-dom'
 import '../styles/createnews.scss'
+import { supabase } from '../supabaseClient';
+import React from 'react'; 
 
 const CreateNews = () => {
+    const [corn, setCorn] = React.useState({
+        set1: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_640.jpg',
+        set2: '',
+        set3: '',
+        set4: '',
+        set5: 'few seconds ago' 
+    })
+
+    console.log(corn, 'corn data')
+
+    const handleCorn = async () => {
+        try {
+          const { data, error } = await supabase
+            .from('news') 
+            .insert([
+                {
+                    image: corn.set1,
+                    preview: corn.set2,
+                    body: corn.set3,
+                    time: corn.set5,
+                    source: corn.set4
+                }
+            ]);
+    
+          if (error) {
+            console.error(error);
+            return;
+          }
+    
+          console.log('Data uploaded successfully:', data);
+        } catch (error) {
+          console.error('Error uploading data:', error.message);
+        }
+      };
+
+      const cornRow = (name, value) => {
+        setCorn({ ...corn, [name]: value });
+      };
+
   return (
     <div className='cn' >
         <h2>Add News</h2>
@@ -13,15 +54,27 @@ const CreateNews = () => {
             </section>
             <section>
                 <h3>Title</h3>
-                <input placeholder='Add news tilte' />
+                <input 
+                placeholder='Add news tilte' 
+                value={corn.set2}
+                onChange={(e) => cornRow('set2', e.target.value)}
+                />
             </section>
             <section>
                 <h3>Category</h3>
-                <input placeholder='Add news category' />
+                <input 
+                placeholder='Add news category' 
+                value={corn.set4}
+                onChange={(e) => cornRow('set4', e.target.value)}
+                />
             </section>
             <section>
                 <h3>Body</h3>
-                <textarea placeholder='Add news body' />
+                <textarea 
+                placeholder='Add news body' 
+                value={corn.set3}
+                onChange={(e) => cornRow('set3', e.target.value)}
+                />
             </section>
         </div>
 
@@ -30,7 +83,10 @@ const CreateNews = () => {
         >
         <button
         className='button-17'
-        onClick={() => alert('News uploaded')}
+        onClick={() => {
+            handleCorn();
+            alert('News uploaded')
+        }}
         >
             Upload
         </button>
